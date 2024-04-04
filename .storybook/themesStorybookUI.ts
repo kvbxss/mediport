af77204743b09910c0ff316571ec8be1715ec5f6
kvbxss <kubadownar444@gmail.com>
2024-04-04 20:22:46 +0200

    added themes

---
+++ .storybook/themesStorybookUI.ts
@@ -0,0 +1,30 @@
+export const darkUIStorybook = {
+  colorPrimary: "hsl(210 40% 98%)",
+  colorSecondary: "hsl(217.2 32.6% 17.5%)",
+
+  appBg: "hsl(222.2 84% 4.9%)",
+  appContentBg: "hsl(222.2 84% 4.9%)",
+  appBorderColor: "hsl(217.2 32.6% 17.5%)",
+  appBorderRadius: ".4rem",
+
+  textColor: "hsl(210 40% 98%)",
+  textInverseColor: "hsl(217.2 32.6% 17.5%)",
+
+  barTextColor: "hsl(210 40% 98%)",
+  barSelectedColor: "hsl(210 40% 98%)",
+  barBg: "hsl(222.2 84% 4.9%)",
+
+  inputBg: "hsl(217.2 32.6% 17.5%)",
+  inputBorder: "hsl(217.2 32.6% 17.5%)",
+  inputTextColor: "hsl(210 40% 98%)",
+  inputBorderRadius: "0.4rem",
+};
+
+export const lightUIStorybook = {};
+
+export const commonTheme = {
+  brandTitle: "Shadcn-ui",
+  brandUrl: "https://ui.shadcn.com/",
+  brandTarget: "_blank",
+  brandImage: "https://ui.shadcn.com/favicon-32x32.png",
+};
